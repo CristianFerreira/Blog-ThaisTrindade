d1e2cdd2a83bafa430b4471ad4a51cbcae1d40d3
Cristian Ferreira <cristianferreira_gks@hotmail.com>
2017-07-19 00:05:57 -0300

    Adicionando angular material

--- BlogTravelWebApp/src/app/app.module.ts
+++ BlogTravelWebApp/src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MaterialModule } from '@angular/material';
+import 'hammerjs';
+
 
 import { AppComponent } from './app.component';
 import { HeadBarComponent } from './components/shared/head-bar/head-bar.component';
 import { SubMenuComponent } from './components/shared/sub-menu/sub-menu.component';
 
+
+
 @NgModule({
   declarations: [
     AppComponent,
     HeadBarComponent,
-    SubMenuComponent
+    SubMenuComponent,
+    MaterialModule
   ],
   imports: [
     BrowserModule
